b8e6d74920495db5a8219149e1ffecbc2d84805d
MatthewHeun <matthew.heun@me.com>
2017-05-17 20:50:49 -0400

    Deleted blank comment line.

--- R/Byname.R
+++ R/Byname.R
@@ -776,7 +776,6 @@ Iminus_byname <- function(m){
 #' DF2[[1, "m2"]] <- m2
 #' DF2[[2, "m2"]] <- m2
 #' DF2 %>% clean_byname(margin = c(1,2), clean_value = -20)
-#' 
 clean_byname <- function(m, margin, clean_value = 0){
   if (1 %in% margin & 2 %in% margin){
     # Clean both dimensions of m.
