935776168d2787539280f65b5f2515a1726ab3a3
MatthewHeun <matthew.heun@me.com>
2017-10-30 21:57:24 -0400

    Completed sums tests.

--- tests/tests_byname.R
+++ tests/tests_byname.R
@@ -5,6 +5,7 @@
 # and tests containing the string "(" don't work as expectged.
 
 library(dplyr)
+library(parallel)
 library(byname)
 library(testthat)
 
@@ -22,57 +23,63 @@ test_that("sums of matrices", {
   industrynames <- c("i1", "i2")
   U <- matrix(1:4, ncol = 2, dimnames = list(commoditynames, industrynames)) %>%
     setrowtype("Commodities") %>% setcoltype("Industries")
-  Y <- matrix(1:4, ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
-    setrowtype("Commodities") %>% setcoltype("Industries")
   V <- matrix(1:4, ncol = 2, dimnames = list(industrynames, commoditynames)) %>%
     setrowtype("Industries") %>% setcoltype("Commodities")
+  Y <- matrix(1:4, ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
+    setrowtype("Commodities") %>% setcoltype("Industries")
   
+  UplusY <- matrix(5, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>%
+    setrowtype(rowtype(U)) %>% setcoltype(coltype(U))
+  
+  Uplus100 <- U + 100
+  
+  DF <- data.frame(U = I(list()), Y = I(list()))
+  DF[[1,"U"]] <- U
+  DF[[2,"U"]] <- U
+  DF[[1,"Y"]] <- Y
+  DF[[2,"Y"]] <- Y
+  
+  # This is a non-sensical test.  Row and column names are not respected. 
+  # Row names, column names, and row and column types come from the first operand (U).
   expect_equal(U + Y, 
-               # This is a non-sensical test.  Row and column names are not respected. 
-               # Row names, column names, and row and column types come from the first operand (U).
                matrix(c(2, 4, 6, 8), ncol = 2, dimnames = dimnames(U)) %>% 
                  setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
-  expect_equal(sum_byname(U, Y),
-               # Now, row and column names are respected.
-               matrix(5, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>%
-                 setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
-  expect_equal(sum_byname(U, 100), U + 100)
-  expect_equal(sum_byname(200, Y), Y + 200 %>% complete_and_sort())
-
-  expect_equal(U + V,
-               # This is a non-sensical test.  Row and column names are not respected.
-               # Row names, column names, and row and column types come from the first operand (U).
-               matrix(c(2, 4, 6, 8), ncol = 2, dimnames = dimnames(U)) %>% 
-                 setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
-
-  # We should not be able to add U and V, because their row and column types differ.
-  expect_error(sum_byname(U, V), as.character("rowtype(a) == rowtype(b) is not TRUE"))
-})
+  # Now, row and column names are respected.
+  expect_equal(sum_byname(U, Y), UplusY)
   
-  #' 
-  #' # This also works with lists
-  #' sum_byname(list(U,U), list(G,G))
-  #' sum_byname(list(U,U), list(100,100))
-  #' sum_byname(list(U,U), as.list(rep_len(100, 2)))
-  #' DF <- data.frame(U = I(list()), G = I(list()))
-  #' DF[[1,"U"]] <- U
-  #' DF[[2,"U"]] <- U
-  #' DF[[1,"G"]] <- G
-  #' DF[[2,"G"]] <- G
-  #' sum_byname(DF$U, DF$G)
-  #' DF %>% mutate(sums = sum_byname(U, G))
-  #' sum_byname(U) # If only one argument, return it.
-  #' sum_byname(2, NULL) # Gives 2
-  #' sum_byname(2, NA)   # Gives NA
-  #' sum_byname(NULL, 1) # Gives 1
-  #' sum_byname(list(NULL, 1), list(1, 1))
-  #' DF2 <- data.frame(U = I(list()), G = I(list()))
-  #' DF2[[1,"U"]] <- NULL
-  #' DF2[[2,"U"]] <- U
-  #' DF2[[1,"G"]] <- G
-  #' DF2[[2,"G"]] <- G
-  #' sum_byname(DF2$U, DF2$G)
-  #' DF3 <- DF2 %>% mutate(sums = sum_byname(U, G))
-  #' DF3
-  #' DF3$sums[[1]]
-  #' DF3$sums[[2]]
+  expect_equal(sum_byname(U, 100), U + 100)
+  expect_equal(sum_byname(200, Y), 200 + Y %>% sort_rows_cols() %>% 
+                                               setrowtype(rowtype(Y)) %>% 
+                                               setcoltype(coltype(Y)))
+  
+  # This is a non-sensical test.  Row and column names are not respected.
+  # Row names, column names, and row and column types come from the first operand (U).
+  expect_equal(U + V,
+               matrix(c(2, 4, 6, 8), ncol = 2, dimnames = dimnames(U)) %>% 
+                 setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
+  
+  # We should not be able to add U and V, because their row and column types differ.
+  # Would like to test for entire error message which is 
+  # "rowtype(a) == rowtype(b) is not TRUE"
+  # However, it seems that the testthat package has trouble dealing with "(" in error messages.
+  # So, we'll just test for the first word.
+  expect_error(sum_byname(U, V), "rowtype")
+  
+  # If only one argument, return it.
+  expect_equal(sum_byname(U), U)
+  # If summed against NULL, return the item.
+  expect_equal(sum_byname(NULL, 1), 1)
+  expect_equal(sum_byname(2, NULL), 2)
+  expect_equal(sum_byname(list(NULL, 1), list(1, 1)), list(1, 2))
+  # If summed against NA, return NA
+  expect_equal(sum_byname(2, NA), NA_integer_)
+  
+  # sum_byname should also work with lists.
+  expect_equal(sum_byname(list(U,U), list(Y, Y)), list(UplusY, UplusY))
+  expect_equal(sum_byname(list(U,U), list(100,100)), list(Uplus100, Uplus100))
+  expect_equal(sum_byname(list(U,U), as.list(rep_len(100, 2))), list(Uplus100, Uplus100))
+  
+  # sum_byname also should work with data frames, as they are lists.
+  expect_equal(sum_byname(DF$U, DF$Y), list(UplusY, UplusY))
+  expect_equal(DF %>% mutate(sums = sum_byname(U, Y)), DF %>% mutate(sums = list(UplusY, UplusY)))
+})
