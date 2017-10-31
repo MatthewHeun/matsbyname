24b8a7a338619d4097a19143265a6ffb3c3b0f11
MatthewHeun <matthew.heun@me.com>
2017-10-31 13:09:25 -0400

    Added differences tests

--- tests/tests_byname.R
+++ tests/tests_byname.R
@@ -21,15 +21,12 @@ V <- matrix(1:4, ncol = 2, dimnames = list(industrynames, commoditynames)) %>%
   setrowtype("Industries") %>% setcoltype("Commodities")
 Y <- matrix(1:4, ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
   setrowtype("Commodities") %>% setcoltype("Industries")
-
-# Define a data frame to be used with testing below.
-DF <- data.frame(U = I(list()), Y = I(list()))
-DF[[1,"U"]] <- U
-DF[[2,"U"]] <- U
-DF[[1,"Y"]] <- Y
-DF[[2,"Y"]] <- Y
+Z <- matrix(rev(1:4), ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
+  setrowtype("Commodities") %>% setcoltype("Industries")
 
 UplusY <- matrix(5, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>%
+  setrowtype(rowtype(U)) %>% setcoltype(coltype(U))
+UminusZ <- matrix(0, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>% 
   setrowtype(rowtype(U)) %>% setcoltype(coltype(U))
 
 Uplus100 <- U + 100
@@ -78,6 +75,13 @@ test_that("sums of matrices", {
 })
 
 test_that("sums of matrices in lists and data frames", {
+  # Define a data frame to be used with testing below.
+  DF <- data.frame(U = I(list()), Y = I(list()))
+  DF[[1,"U"]] <- U
+  DF[[2,"U"]] <- U
+  DF[[1,"Y"]] <- Y
+  DF[[2,"Y"]] <- Y
+  
   # sum_byname should also work with lists.
   expect_equal(sum_byname(list(U,U), list(Y, Y)), list(UplusY, UplusY))
   expect_equal(sum_byname(list(U,U), list(100,100)), list(Uplus100, Uplus100))
@@ -101,31 +105,42 @@ test_that("differences of constants", {
   # If summed against NA, return NA
   expect_equal(difference_byname(2, NA), NA_integer_)
 })
+
+test_that("differences of matrices", {
+  # If only one argument, return it.
+  expect_equal(difference_byname(U), U)
   
+  # This is a non-sensical test.  Row and column names are not respected. 
+  # Row names, column names, and row and column types come from the first operand (U).
+  expect_equal(U - Z, matrix(c(-3, -1, 1, 3), nrow = 2, dimnames = dimnames(U)) %>% 
+                      setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
+               
+  # Row and column names respected! Should be all zeroes.
+  expect_equal(difference_byname(U, Z), matrix(0, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>% 
+                                        setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
+  expect_equal(difference_byname(100, U), matrix(c(99, 98, 97, 96), nrow = 2, dimnames = dimnames(U)) %>% 
+                 setrowtype(rowtype(U)) %>% setcoltype(coltype(U)))
+  # difference_byname should sort the rows and column names.
+  # So we expect the dimnames of the difference to be the same as the dimnames of U (which has sorted dimnames).
+  expect_equal(difference_byname(10, Z), matrix(c(9, 8, 7, 6), ncol = 2, dimnames = dimnames(U)) %>% 
+                                        setrowtype(rowtype(Z)) %>% setcoltype(coltype(Z)))
+  # When subtrahend is missing, return minuend (in this case, Z).
+  expect_equal(difference_byname(Z), Z)
+  # When minuend is missing, return - subtrahend (in this case, -Z)
+  expect_equal(difference_byname(subtrahend = Z), elementproduct_byname(-1, Z))
+})
+  
+test_that("differences of matrices in lists and data frames", {
+  # Define a data frame to be used with testing below.
+  DF <- data.frame(U = I(list()), Z = I(list()))
+  DF[[1,"U"]] <- U
+  DF[[2,"U"]] <- U
+  DF[[1,"Z"]] <- Z
+  DF[[2,"Z"]] <- Z
 
-
-
-
-#' 
-#' commoditynames <- c("c1", "c2")
-#' industrynames <- c("i1", "i2")
-#' U <- matrix(1:4, ncol = 2, dimnames = list(commoditynames, industrynames)) %>%
-#'   setrowtype("Commodities") %>% setcoltype("Industries")
-#' G <- matrix(rev(1:4), ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
-#'   setrowtype("Commodities") %>% setcoltype("Industries")
-#' U - G # Non-sensical. Row and column names not respected.
-#' difference_byname(U, G) # Row and column names respected! Should be all zeroes.
-#' difference_byname(100, U)
-#' difference_byname(10, G)
-#' difference_byname(G) # When subtrahend is missing, return minuend (in this case, G).
-#' difference_byname(subtrahend = G) # When minuend is missing, return - subtrahend (in this case, -G)
-#' # This also works with lists
-#' difference_byname(list(100, 100), list(50, 50))
-#' difference_byname(list(U,U), list(G,G))
-#' DF <- data.frame(U = I(list()), G = I(list()))
-#' DF[[1,"U"]] <- U
-#' DF[[2,"U"]] <- U
-#' DF[[1,"G"]] <- G
-#' DF[[2,"G"]] <- G
-#' difference_byname(DF$U, DF$G)
-#' DF %>% mutate(diffs = difference_byname(U, G))
+  expect_equal(difference_byname(list(100, 100), list(50, 50)), list(50, 50))
+  expect_equal(difference_byname(list(U, U), list(Z, Z)), list(UminusZ, UminusZ))
+  expect_equal(difference_byname(DF$U, DF$Z), list(UminusZ, UminusZ))
+  expect_equal(difference_byname(DF %>% mutate(diffs = difference_byname(U, Z))), DF %>% mutate(diffs = list(UminusZ)))
+  
+})
