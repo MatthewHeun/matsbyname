343bfa7cb2421edbbf959d5c556e0ff1c7539ae8
MatthewHeun <matthew.heun@me.com>
2017-10-30 22:28:43 -0400

    Added difference_byname test. Not working.

--- tests/tests_byname.R
+++ tests/tests_byname.R
@@ -11,33 +11,44 @@ library(testthat)
 
 context("Sums")
 
+# Define some matrices with commodity and industry names and types
+# These matrices will be used in the tests below.
+commoditynames <- c("c1", "c2")
+industrynames <- c("i1", "i2")
+U <- matrix(1:4, ncol = 2, dimnames = list(commoditynames, industrynames)) %>%
+  setrowtype("Commodities") %>% setcoltype("Industries")
+V <- matrix(1:4, ncol = 2, dimnames = list(industrynames, commoditynames)) %>%
+  setrowtype("Industries") %>% setcoltype("Commodities")
+Y <- matrix(1:4, ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
+  setrowtype("Commodities") %>% setcoltype("Industries")
+
+# Define a data frame to be used with testing below.
+DF <- data.frame(U = I(list()), Y = I(list()))
+DF[[1,"U"]] <- U
+DF[[2,"U"]] <- U
+DF[[1,"Y"]] <- Y
+DF[[2,"Y"]] <- Y
+
+UplusY <- matrix(5, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>%
+  setrowtype(rowtype(U)) %>% setcoltype(coltype(U))
+
+Uplus100 <- U + 100
+
 test_that("sums of constants", {
-  
   # Simple sum of constants
   expect_equal(sum_byname(2, 3), 5)
+  
+  # If summed against NULL, return the item.
+  expect_equal(sum_byname(NULL, 1), 1)
+  expect_equal(sum_byname(2, NULL), 2)
+  expect_equal(sum_byname(list(NULL, 1), list(1, 1)), list(1, 2))
+  # If summed against NA, return NA
+  expect_equal(sum_byname(2, NA), NA_integer_)
 })
 
 test_that("sums of matrices", {
-  # Matrices with commodity and industry names and types
-  commoditynames <- c("c1", "c2")
-  industrynames <- c("i1", "i2")
-  U <- matrix(1:4, ncol = 2, dimnames = list(commoditynames, industrynames)) %>%
-    setrowtype("Commodities") %>% setcoltype("Industries")
-  V <- matrix(1:4, ncol = 2, dimnames = list(industrynames, commoditynames)) %>%
-    setrowtype("Industries") %>% setcoltype("Commodities")
-  Y <- matrix(1:4, ncol = 2, dimnames = list(rev(commoditynames), rev(industrynames))) %>%
-    setrowtype("Commodities") %>% setcoltype("Industries")
-  
-  UplusY <- matrix(5, nrow = 2, ncol = 2, dimnames = dimnames(U)) %>%
-    setrowtype(rowtype(U)) %>% setcoltype(coltype(U))
-  
-  Uplus100 <- U + 100
-  
-  DF <- data.frame(U = I(list()), Y = I(list()))
-  DF[[1,"U"]] <- U
-  DF[[2,"U"]] <- U
-  DF[[1,"Y"]] <- Y
-  DF[[2,"Y"]] <- Y
+  # If only one argument, return it.
+  expect_equal(sum_byname(U), U)
   
   # This is a non-sensical test.  Row and column names are not respected. 
   # Row names, column names, and row and column types come from the first operand (U).
@@ -64,16 +75,9 @@ test_that("sums of matrices", {
   # However, it seems that the testthat package has trouble dealing with "(" in error messages.
   # So, we'll just test for the first word.
   expect_error(sum_byname(U, V), "rowtype")
-  
-  # If only one argument, return it.
-  expect_equal(sum_byname(U), U)
-  # If summed against NULL, return the item.
-  expect_equal(sum_byname(NULL, 1), 1)
-  expect_equal(sum_byname(2, NULL), 2)
-  expect_equal(sum_byname(list(NULL, 1), list(1, 1)), list(1, 2))
-  # If summed against NA, return NA
-  expect_equal(sum_byname(2, NA), NA_integer_)
-  
+})
+
+test_that("sums of matrices in lists and data frames", {
   # sum_byname should also work with lists.
   expect_equal(sum_byname(list(U,U), list(Y, Y)), list(UplusY, UplusY))
   expect_equal(sum_byname(list(U,U), list(100,100)), list(Uplus100, Uplus100))
@@ -86,12 +90,15 @@ test_that("sums of matrices", {
 
 context("Differences")
 
+test_that("differences of constants", {
+  expect_that(difference_byname(100, 50), 50)
+})
+  
 
 
 
 
-
-#' difference_byname(100, 50)
+#' 
 #' commoditynames <- c("c1", "c2")
 #' industrynames <- c("i1", "i2")
 #' U <- matrix(1:4, ncol = 2, dimnames = list(commoditynames, industrynames)) %>%
