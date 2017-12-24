56600d9b878a88fafacfd43b88baf5dca7d5fea2
MatthewHeun <matthew.heun@me.com>
2017-12-24 16:08:40 -0500

    Now adding keyword array

--- R/Byname.R
+++ R/Byname.R
@@ -11,6 +11,7 @@ library(dplyr)
 #' Zeroes are inserted for missing matrix elements.
 #'
 #' @return A matrix representing the name-wise sum of \code{addend} and \code{augend}
+#' 
 #' @export
 #'
 #' @examples
