5c11416eb43611ac0fdf271c6380df351d60383e
Matthew Heun <matthew.heun@me.com>
2020-04-20 10:52:12 -0400

    Deleted unused buggy (and previously-commented) lines.

--- R/Utilities.R
+++ R/Utilities.R
@@ -744,8 +744,6 @@ clean_byname <- function(a, margin = c(1, 2), clean_value = 0){
     keepcols <- apply(b, 2, function(x) {
       !all(x == clean_value)
     })
-    # keepcolnames <- names(which(keepcols))
-    # c <- select_cols_byname(a = b, retain_pattern = make_pattern(row_col_names = keepcolnames, pattern_type = "exact"))
     cleaned <- b[ , keepcols, drop = FALSE]
     if (margin == 1) {
       return(transpose_byname(cleaned))
