49d2c50c23949c0b274d337285c938f38f485d7d
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-09-13 19:01:17 -0400

    Another test for trim_rows_cols()

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -1,9 +1,7 @@
-test_that("trim_rowscols() works with NULL a", {
+test_that("trim_rows_cols() works with NULL a", {
   mat <- matrix(1, nrow = 1, ncol = 1)
   res <- trim_rows_cols(NULL, mat)
   expect_null(res)
-  
-  
 })
 
 
@@ -19,8 +17,18 @@ test_that("trim_rows_cols() works as expected", {
                 dimnames = list(c("r1", "bogus"), c("c1", "bogus", "c2"))) %>% 
     setrowtype("rowtype") %>% setcoltype("coltype")
   
-  res <- trim_rows_cols(a, mat, margin = 1)
-  expect_equal(res, matrix(c(1, 2, 3), nrow = 1, ncol = 3, byrow = TRUE, 
-                           dimnames = list(c("r1"), c("c1", "c2", "c3"))) %>% 
+  # Test trimming rows
+  res1 <- trim_rows_cols(a, mat, margin = 1)
+  expect_equal(res1, matrix(c(1, 2, 3), nrow = 1, ncol = 3, byrow = TRUE, 
+                            dimnames = list(c("r1"), c("c1", "c2", "c3"))) %>% 
                  setrowtype("rowtype") %>% setcoltype("coltype"))
+  
+  # Test trimming cols
+  res2 <- trim_rows_cols(a, mat, margin = 2)
+  expect_equal(res2, matrix(c(1, 2, 
+                              4, 5, 
+                              7, 8), nrow = 3, ncol = 2, byrow = TRUE, 
+                            dimnames = list(c("r1", "r2", "r3"), c("c1", "c2"))) %>% 
+                 setrowtype("rowtype") %>% setcoltype("coltype"))
+  
 })
