f4c4927902f6e929d01cbea9538ee9a43ae02f95
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-09-13 19:55:18 -0400

    More tests for trim_rows_cols

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -1,11 +1,36 @@
-test_that("trim_rows_cols() works with NULL a", {
-  mat <- matrix(1, nrow = 1, ncol = 1)
-  res <- trim_rows_cols(NULL, mat)
-  expect_null(res)
+test_that("trim_rows_cols() works in degenerate cases", {
+  # When a is NULL, we expect NULL as a result
+  a <- matrix(c(1, 2, 3, 
+                4, 5, 6, 
+                7, 8, 9), nrow = 3, ncol = 3, byrow = TRUE, 
+              dimnames = list(c("r1", "r2", "r3"), c("c1", "c2", "c3")))
+  mat <- matrix(c(1, 2,
+                  3, 4), nrow = 2, ncol = 2, byrow = TRUE, 
+                dimnames = list(c("r1", "r3"), c("c1", "c3")))
+
+  expect_null(trim_rows_cols(NULL, mat))
+  
+  # When mat is NULL, we expect a returned unmodified
+  expect_equal(trim_rows_cols(a, NULL), a)
+  
+  # When mat has NULL dimnames, a is returned unmodified
+  mat2 <- mat
+  dimnames(mat2) <- NULL
+  expect_warning(res <- trim_rows_cols(a, mat2), "NULL names in trim_rows_cols, despite 'mat' being specified. Returning 'a' unmodified.")
+  expect_equal(res, a)
+  
+  # `mat` has `NULL` for dimnames on `margin`, an error is returned.
+  mat3 <- mat
+  dimnames(mat3) <- list(c("r1", "r3"), NULL)
+  expect_error(trim_rows_cols(a, mat3), "NULL dimnames for margin = 2 on 'mat")
+
+  mat4 <- mat
+  dimnames(mat4) <- list(NULL, c("c1", "c3"))
+  expect_error(trim_rows_cols(a, mat4), "NULL dimnames for margin = 1 on 'mat")
 })
 
 
-test_that("trim_rows_cols() works as expected", {
+test_that("trim_rows_cols() works as expected with single matrices", {
   a <- matrix(c(1, 2, 3, 
                 4, 5, 6, 
                 7, 8, 9), nrow = 3, ncol = 3, byrow = TRUE, 
