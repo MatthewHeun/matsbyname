1700973a7abf1a55b3522da995fb30e050ab55bc
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-09-13 20:08:22 -0400

    More tests.

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -61,5 +61,30 @@ test_that("trim_rows_cols() works as expected with single matrices", {
   expect_equal(res3, matrix(c(1, 2), nrow = 1, ncol = 2, byrow = TRUE, 
                             dimnames = list(c("r1"), c("c1", "c2"))) %>% 
                  setrowtype("rowtype") %>% setcoltype("coltype"))
+})
+
+
+test_that("trim_rows_cols() works when a list is given", {
+  a <- matrix(c(1, 2, 3, 
+                4, 5, 6, 
+                7, 8, 9), nrow = 3, ncol = 3, byrow = TRUE, 
+              dimnames = list(c("r1", "r2", "r3"), c("c1", "c2", "c3"))) %>% 
+    setrowtype("rowtype") %>% setcoltype("coltype")
   
+  mat <- matrix(c(1, 2, 3,
+                  4, 5, 6), nrow = 2, ncol = 3, byrow = TRUE, 
+                dimnames = list(c("r1", "bogus"), c("c1", "bogus", "c2"))) %>% 
+    setrowtype("rowtype") %>% setcoltype("coltype")
+  
+  a_list <- list(a, a)
+  
+  res <- matrix(c(1, 2), nrow = 1, ncol = 2, byrow = TRUE, 
+                dimnames = list(c("r1"), c("c1", "c2"))) %>% 
+    setrowtype("rowtype") %>% setcoltype("coltype")
+  
+  expect_equal(trim_rows_cols(a_list, mat, margin = list(c(1,2), c(1,2))), 
+               list(res, res))
+  
+  expect_equal(trim_rows_cols(a_list, list(mat, mat), margin = list(c(1,2), c(1,2))), 
+               list(res, res))
 })
