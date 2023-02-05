d5d69c0b57b0c0ae790b18399d440cdfdb68d030
Matthew Heun <matthew.heun@me.com>
2023-02-05 15:52:57 -0500

    Another test.

--- tests/testthat/test-complete_and_sort.R
+++ tests/testthat/test-complete_and_sort.R
@@ -211,6 +211,24 @@ test_that("complete_and_sort() preserves row and column types", {
 
 
 test_that("complete_and_sort() works with Matrix objects", {
-  
+  A <- Matrix::Matrix(1, nrow = 2, ncol = 2, dimnames = list(c("r1", "r2"), c("c1", "c2")))
+  B <- Matrix::Matrix(1, nrow = 2, ncol = 2, dimnames = list(c("r3", "r4"), c("c3", "c4")))
+  res1 <- complete_and_sort(A, B)
+  expectedA <- Matrix::Matrix(c(1, 1, 0, 0,
+                                1, 1, 0, 0, 
+                                0, 0, 0, 0,
+                                0, 0, 0, 0),
+                              nrow = 4, ncol = 4, byrow = TRUE, 
+                              dimnames = list(c("r1", "r2", "r3", "r4"), 
+                                              c("c1", "c2", "c3", "c4")))
+  expectedB <- Matrix::Matrix(c(0, 0, 0, 0,
+                                0, 0, 0, 0, 
+                                0, 0, 1, 1,
+                                0, 0, 1, 1),
+                              nrow = 4, ncol = 4, byrow = TRUE, 
+                              dimnames = list(c("r1", "r2", "r3", "r4"), 
+                                              c("c1", "c2", "c3", "c4")))
+  expect_equal(res1$a, expectedA)
+  expect_equal(res1$b, expectedB)
 })
 
