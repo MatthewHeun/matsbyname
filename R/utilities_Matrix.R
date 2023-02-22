7193db91de7c527898f631d67187d248ef38c9d9
Matthew Heun <matthew.heun@me.com>
2023-02-22 13:19:55 -0500

    Better docs.

--- R/utilities_Matrix.R
+++ R/utilities_Matrix.R
@@ -134,6 +134,23 @@ is.Matrix <- function(a) {
 }
 
 
+#' Tells whether an object is one of a matrix or a Matrix
+#' 
+#' Often, it helps to know whether an object is a `matrix` or a `Matrix`, 
+#' and you don't care which. 
+#' This function helps in those situations.
+#'
+#' @param a The object about which we want to know if it is a `matrix` or a `Matrix`.
+#'
+#' @return `TRUE` when `a` is a `matrix` or a `Matrix`. `FALSE` otherwise.
+#' 
+#' @export
+#'
+#' @examples
+#' is_matrix_or_Matrix(42)
+#' is_matrix_or_Matrix(matrix(42))
+#' is_matrix_or_Matrix(Matrix::Matrix(42))
+#' is_matrix_or_Matrix(matsbyname::Matrix(42))
 is_matrix_or_Matrix <- function(a) {
   is.matrix(a) | is.Matrix(a)
 }
