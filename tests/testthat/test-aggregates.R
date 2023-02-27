c5e90b1bc0ba4b6c868df8b54b6c1adeab578f6a
Matthew Heun <matthew.heun@me.com>
2023-02-27 10:20:51 -0500

    Added a test for a bug.

--- tests/testthat/test-aggregates.R
+++ tests/testthat/test-aggregates.R
@@ -867,3 +867,17 @@ test_that("aggregate_pieces_byname() works with repeated row labels not in aggre
                         1,  4), byrow = TRUE, nrow = 2, ncol = 2, dimnames = list(c("c", "f"), c("c1", "c2")))
   expect_equal(res2, expected2)
 })
+
+
+test_that("aggregate_pieces_byname() works with 2 notations", {
+  a <- matrix(1:3, nrow = 3, ncol = 1, dimnames = list(c("a [from b]", "c -> d", "c -> e"), "c1"))
+  res1 <- a %>% 
+    aggregate_pieces_byname(piece = "noun")
+  expected1 <- matrix(c(1, 5), nrow = 2, ncol = 1, dimnames = list(c("a", "c"), "c1"))
+  expect_equal(res1, expected1)
+  
+  res2 <- a %>% 
+    aggregate_pieces_byname(piece = "noun", margin = 1, notation = c(RCLabels::bracket_notation, RCLabels::arrow_notation), inf_notation = TRUE)
+  expected2 <- expected1
+  expect_equal(res2, expected2)
+})
