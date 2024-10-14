818f3de7d61b40ac7b972829c724ec44fecaf57f
Matthew Heun <matthew.heun@me.com>
2024-10-14 09:24:36 -0400

    Now throwing warnings or errors.

--- R/reallocate.R
+++ R/reallocate.R
@@ -8,24 +8,14 @@
 #' 
 #' This function will provide answers, but 
 #' it is unlikely that the answers will be meaningful, when the
-#' remaining data (the rows or columns not being allocated)
+#' remaining data (the rows or columns not being distributed)
 #' contain negative numbers.
-#' 
-#' When the remaining rows or columns not being reallocated
-#' contain zeroes, the result is determined by `.zero_behaviour`.
-#' Options are one of:
-#' * "error" (the default) to throw an error.
-#' * "warning" to issue a warning but continue execution. Be careful with this option!
-#' * "evenly distribute" to evenly distribute across remaining rows or columns.
 #'
 #' @param a A matrix or a list of matrices.
 #' @param rowcolnames The names of the rows or columns to be redistributed.
 #' @param margin The margin of the matrix on which the `rowcolnames` are located.
 #'               Default is `c(1, 2)`, meaning that both rows (`1`) and columns (`2`)
 #'               will be checked for `rowcolnames` and redistributed.
-#' @param .zero_behaviour Tells how to proceed when remaining rows or columns are all zero.
-#'                        Default behaviour is to throw an error.
-#'                        See details.
 #' @param pattern_type The pattern type desired. Default is "exact". 
 #'                     Other options are "leading", "trailing", "anywhere", 
 #'                     and "literal". 
@@ -49,37 +39,35 @@
 #'             dimnames = list(c("r1", "r2", "r3"), 
 #'             c("c1", "c2", "c3")))
 #' m
-#' # Move row 3 into the other rows (r1 and r2) proportionally
+#' # Move row 3 into the other rows proportionally
 #' reallocate_byname(m, rowcolnames = "r3", margin = 1)
-#' # Move column 2 into the other columns (c1 and c3) proportionally
+#' # Move column 2 into the other columns proportionally
 #' reallocate_byname(m, rowcolnames = "c2", margin = 2)
 reallocate_byname <- function(a, 
-                              rowcolnames = NULL,
-                              margin = c(1, 2), 
-                              .zero_behaviour = c("error", "warning"),
-                              piece = "all", 
-                              pattern_type = "exact", 
-                              prepositions = RCLabels::prepositions_list, 
-                              notation = RCLabels::notations_list) {
+                                rowcolnames = NULL,
+                                margin = c(1, 2), 
+                                piece = "all", 
+                                pattern_type = "exact", 
+                                prepositions = RCLabels::prepositions_list, 
+                                notation = RCLabels::notations_list) {
   margin <- prep_vector_arg(a, margin)
-  .zero_behaviour <- match.arg(.zero_behaviour, several.ok = FALSE)
-  reallocate_func <- function(a_mat, margin) {
+  redistribute_func <- function(a_mat, margin) {
     if (length(margin) != length(unique(margin))) {
-      stop("margin must contain unique integers in matsbyname::reallocate_byname()")
+      stop("margin must contain unique integers in matsbyname::redistribute_byname()")
     }
     if (!length(margin) %in% c(1,2)) {
-      stop("margin must have length 1 or 2 in matsbyname::reallocate_byname()")
+      stop("margin must have length 1 or 2 in matsbyname::redistribute_byname()")
     }
     
     if (!all(sapply(margin, function(mar) {mar %in% c(1,2)}))) {
-      stop("margin must be 1, 2, or c(1, 2) in matsbyname::reallocate_byname()")
+      stop("margin must be 1, 2, or c(1, 2) in matsbyname::redistribute_byname()")
     }
 
     out <- a_mat
     if (2 %in% margin) {
       out <- out |> 
         matsbyname::transpose_byname() |> 
-        reallocate_func(margin = 1) |> 
+        redistribute_func(margin = 1) |> 
         matsbyname::transpose_byname()
     }
     if (1 %in% margin) {
@@ -122,16 +110,10 @@ reallocate_byname <- function(a,
       if (any(problem_cols)) {
         names_problem_cols <- names(problem_cols[which(problem_cols)])
         rows_or_cols <- ifelse(margin == 1, "rows", "columns")
-        msg <- paste0("The following cannot be reallocated due to all zero values in the receiving ", 
-                      rows_or_cols, 
-                      ": ", 
-                      paste0(names_problem_cols, collapse = ", "))
-        if (.zero_behaviour == "error") {
-          stop(msg)
-        }
-        if (.zero_behaviour == "warning") {
-          warning(msg)  
-        }
+        warning(paste0("The following cannot be redistributed due to all zero values in the receiving ", 
+                       rows_or_cols, 
+                       ": ", 
+                       paste0(names_problem_cols, collapse = ", ")))
       }
       
       # Calculate fractions of redistribution that should be placed in each keeprows
@@ -147,7 +129,7 @@ reallocate_byname <- function(a,
     
     return(out)
   }
-  unaryapply_byname(reallocate_func, a = a,
+  unaryapply_byname(redistribute_func, a = a,
                     .FUNdots = list(margin = margin), 
                     rowcoltypes = "all")
 }
--- tests/testthat/test-reallocate.R
+++ tests/testthat/test-reallocate.R
@@ -1,20 +1,20 @@
-test_that("redistribute_byname() errors as expected", {
+test_that("reallocate_byname() errors as expected", {
   expect_error(
-    matsbyname::redistribute_byname(a = 42, margin = c(1, 2, 3)), 
-    "margin must have length 1 or 2 in matsbyname::redistribute_byname\\(\\)"
+    matsbyname::reallocate_byname(a = 42, margin = c(1, 2, 3)), 
+    "margin must have length 1 or 2 in matsbyname::reallocate_byname\\(\\)"
   )
   expect_error(
-    matsbyname::redistribute_byname(a = 42, margin = 4), 
-    "margin must be 1, 2, or c\\(1, 2\\) in matsbyname::redistribute_byname\\(\\)"
+    matsbyname::reallocate_byname(a = 42, margin = 4), 
+    "margin must be 1, 2, or c\\(1, 2\\) in matsbyname::reallocate_byname\\(\\)"
   )
   expect_error(
-    matsbyname::redistribute_byname(a = 42, margin = c(1, 1)), 
-    "margin must contain unique integers in matsbyname::redistribute_byname\\(\\)"
+    matsbyname::reallocate_byname(a = 42, margin = c(1, 1)), 
+    "margin must contain unique integers in matsbyname::reallocate_byname\\(\\)"
   )
 })
 
 
-test_that("redistribute_byname() works with row redistribution", {
+test_that("reallocate_byname() works with row redistribution", {
   a <- matrix(c(1, 2, 
                 3, 4, 
                 5, 6), 
@@ -22,7 +22,7 @@ test_that("redistribute_byname() works with row redistribution", {
               dimnames = list(c("r1", "r2", "r3"), 
                               c("c1", "c2")))
   
-  res <- matsbyname::redistribute_byname(a, "r3", margin = 1)
+  res <- matsbyname::reallocate_byname(a, "r3", margin = 1)
   
   expected <- matrix(c(1 + 1/4*5, 2 + 2/6*6, 
                        3 + 3/4*5, 4 + 4/6*6), 
@@ -32,7 +32,7 @@ test_that("redistribute_byname() works with row redistribution", {
   expect_equal(res, expected)
   
   # Redistribute 2 rows into 1, essentially providing a summation
-  res2 <- matsbyname::redistribute_byname(a, c("r1", "r3"), margin = 1)
+  res2 <- matsbyname::reallocate_byname(a, c("r1", "r3"), margin = 1)
   
   expected2 <- matrix(c(9, 12), 
                       nrow = 1, ncol = 2, byrow = TRUE, 
@@ -41,20 +41,20 @@ test_that("redistribute_byname() works with row redistribution", {
   expect_equal(res2, expected2)
   
   # Test with a list
-  res3 <- matsbyname::redistribute_byname(list(a, a), "r3", margin = 1)
+  res3 <- matsbyname::reallocate_byname(list(a, a), "r3", margin = 1)
   expected3 <- list(expected, expected)
   expect_equal(res3, expected3)
 })
 
 
-test_that("redistribute_byname() works with column redistribution", {
+test_that("reallocate_byname() works with column redistribution", {
   a <- matrix(c(1, 2, 3,
                 4, 5, 6), 
               nrow = 2, ncol = 3, byrow = TRUE, 
               dimnames = list(c("r1", "r2"), 
                               c("c1", "c2", "c3")))
   
-  res <- matsbyname::redistribute_byname(a, "c3", margin = 2)
+  res <- matsbyname::reallocate_byname(a, "c3", margin = 2)
   
   expected <- matrix(c(1 + 1/3*3, 2 + 2/3*3, 
                        4 + 4/9*6, 5 + 5/9*6), 
@@ -64,7 +64,7 @@ test_that("redistribute_byname() works with column redistribution", {
   expect_equal(res, expected)
   
   # Redistribute 2 columns into 1, essentially providing a summation
-  res2 <- matsbyname::redistribute_byname(a, c("c1", "c2"), margin = 2)
+  res2 <- matsbyname::reallocate_byname(a, c("c1", "c2"), margin = 2)
   
   expected2 <- matrix(c(6, 
                         15), 
@@ -74,21 +74,21 @@ test_that("redistribute_byname() works with column redistribution", {
   expect_equal(res2, expected2)
   
   # Test with a list
-  res3 <- matsbyname::redistribute_byname(list(a, a, a), "c3", margin = 2)
+  res3 <- matsbyname::reallocate_byname(list(a, a, a), "c3", margin = 2)
   expected3 <- list(expected, expected, expected)
   expect_equal(res3, expected3)
 })
 
 
-test_that("redistribute_byname() works with a 0 column", {
-  # Try redistributing a 0 column.
+test_that("reallocate_byname() works with a 0 column", {
+  # Try reallocating a 0 column.
   a <- matrix(c(1, 2, 3, 0,
                 4, 5, 6, 0), 
               nrow = 2, ncol = 4, byrow = TRUE, 
               dimnames = list(c("r1", "r2"), 
                               c("c1", "c2", "c3", "c4")))
   
-  res <- matsbyname::redistribute_byname(a, "c4", margin = 2)
+  res <- matsbyname::reallocate_byname(a, "c4", margin = 2)
   expect_equal(res, matsbyname::select_cols_byname(a, remove_pattern = "c4", fixed = TRUE))
   
   # Try redistributing a non-zero value
@@ -99,19 +99,23 @@ test_that("redistribute_byname() works with a 0 column", {
                nrow = 3, ncol = 2, byrow = TRUE, 
                dimnames = list(c("r1", "r2", "r3"), 
                                c("c1", "c2")))
-  res2 <- matsbyname::redistribute_byname(a2, "r3", margin = 1) |> 
-    expect_warning("The following cannot be redistributed due to all zero values in the receiving rows: c2")
-  expected2 <- matrix(c(2, 0,
+  res2 <- matsbyname::reallocate_byname(a2, "r3", margin = 1) |> 
+    expect_error("The following cannot be reallocated due to all zero values in the receiving rows: c2")
+
+  expected3 <- matrix(c(2, 0,
                         4, 0), 
                       nrow = 2, ncol = 2, byrow = TRUE, 
                       dimnames = list(c("r1", "r2"), 
                                       c("c1", "c2")))
-  expect_equal(res2, expected2)
-
+  res3 <- matsbyname::reallocate_byname(a2, "r3", margin = 1, .zero_behaviour = "warning") |> 
+    expect_equal(expected3) |> 
+    expect_warning("The following cannot be reallocated due to all zero values in the receiving rows: c2")
   
+
 })
 
 
 # Test when row or column is missing
 # Test with row col name pieces
 # Test with different row and col notation
+
