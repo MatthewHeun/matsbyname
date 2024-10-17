5310c781491efccd31c452c8e6cd0fa8efc2869e
Matthew Heun <matthew.heun@me.com>
2024-10-17 11:16:21 -0400

    Improved line breaks in a test.

--- tests/testthat/test-reallocate.R
+++ tests/testthat/test-reallocate.R
@@ -208,7 +208,9 @@ test_that("reallocate_byname() works when choosing by pieces of column names", {
   
   # Specify notation as bracket_notation.
   # Specifying from_notation will not work.
-  a_reallocated3 <- reallocate_byname(a, "b", piece = "from", notation = RCLabels::bracket_notation)
+  a_reallocated3 <- reallocate_byname(a, "b", 
+                                      piece = "from", 
+                                      notation = RCLabels::bracket_notation)
   expected3 <- matrix(c(2 + 2/6*4, 4 + 4/6*4,
                         6 + 6/14*12, 8 + 8/14*12), byrow = TRUE, nrow = 2, ncol = 2,
                       dimnames = list(c("r1", "r2"), c("a [from c]", "e")))
